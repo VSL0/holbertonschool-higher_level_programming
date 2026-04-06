a367237c8997b4b6ce893c0081237f68a7a994b6
VSL0 <VSL0@users.noreply.github.com>
2026-04-06 19:12:16 +0000

    Task 10: Best score

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+
+    best_key = None
+    max_score = 0
+
+    for key, value in a_dictionary.items():
+        if best_key is None or value > max_score:
+            max_score = value
+            best_key = key
+    return best_key
