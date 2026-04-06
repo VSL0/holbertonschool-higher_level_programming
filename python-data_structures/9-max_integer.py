e3c26cb9785683788d8a8bd132fdabd2fa12d54a
VSL0 <VSL0@users.noreply.github.com>
2026-04-06 19:02:29 +0000

    Task 9: Fix PEP8 W293 whitespace

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+"""Module to find the max integer in a list"""
+
+
 def max_integer(my_list=[]):
+    """Finds the biggest integer of a list."""
     if not my_list:
         return None
-    
+
     max_val = my_list[0]
     for i in my_list:
         if i > max_val:
