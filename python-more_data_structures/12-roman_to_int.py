6a98dd8273b3ef4fd019305d1a43d8cf0d791070
VSL0 <VSL0@users.noreply.github.com>
2026-04-06 19:13:56 +0000

    Task 12: Fix PEP8 E501 line length

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Module for Roman to Integer conversion"""
+
+
 def roman_to_int(roman_string):
+    """Converts a Roman numeral to an integer"""
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
 
@@ -10,7 +14,9 @@ def roman_to_int(roman_string):
     total = 0
     for i in range(len(roman_string)):
         val = roman_dict.get(roman_string[i], 0)
-        if i + 1 < len(roman_string) and val < roman_dict.get(roman_string[i + 1], 0):
+        # Check if the next value is larger (subtraction rule)
+        if i + 1 < len(roman_string) and \
+           val < roman_dict.get(roman_string[i + 1], 0):
             total -= val
         else:
             total += val
