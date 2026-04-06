eca32c36040bdd824679baaaab26c710ce520ea8
VSL0 <VSL0@users.noreply.github.com>
2026-04-06 18:49:57 +0000

    Fix: Move 4-hidden_discovery.py to /tmp and ensure script execution

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ import hidden_4
 
 if __name__ == "__main__":
     for name in sorted(dir(hidden_4)):
-        if name[:2] != "__":
+        if not name.startswith("__"):
             print("{}".format(name))
